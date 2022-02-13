c91c57dd4784559838c600a6664d57f09c27df52
batiaev <a.batyaev@gmail.com>
2022-02-13 16:23:37 +0000

    init spring

---
+++ src/main/java/com/batiaev/provisioner/ProvisionerApplication.java
@@ -0,0 +1,13 @@
+package com.batiaev.provisioner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvisionerApplication.class, args);
+    }
+
+}
